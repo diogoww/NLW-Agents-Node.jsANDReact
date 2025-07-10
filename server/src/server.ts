7ff9cdf5ee510cf21a734211543ad6e84519ddc9
Diogo Varaschin <diogovaraschin@outlook.com>
2025-07-10 11:35:04 -0300

    feat: create get-room-questions.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,8 @@ import {
 } from 'fastify-type-provider-zod';
 import { env } from './env.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
+import { createRoomRoute } from './http/routes/create-room.ts';
+import { getRoomQuestions } from './http/routes/get-room-questions.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.register(fastifyCors, {
@@ -21,6 +23,8 @@ app.get('/health', () => {
 });
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
+app.register(getRoomQuestions)
 
 app.listen({ port: env.PORT });
 
