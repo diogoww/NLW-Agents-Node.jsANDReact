9580ce9432c058a97993980aa7f4e66421bedccf
Diogo Varaschin <diogovaraschin@outlook.com>
2025-07-10 15:11:09 -0300

    feat: add create question, create room and get room questions

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,7 @@ import { env } from './env.ts';
 import { getRoomsRoute } from './http/routes/get-rooms.ts';
 import { createRoomRoute } from './http/routes/create-room.ts';
 import { getRoomQuestions } from './http/routes/get-room-questions.ts';
+import { createQuestionRoute } from './http/routes/create-questions.ts';
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.register(fastifyCors, {
@@ -25,6 +26,7 @@ app.get('/health', () => {
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomQuestions)
+app.register(createQuestionRoute)
 
 app.listen({ port: env.PORT });
 
