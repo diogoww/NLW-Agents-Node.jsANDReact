5b7a054a6185ad9d1c8a2aeb02987f0d2e3132f5
Diogo Varaschin <diogovaraschin@outlook.com>
2025-07-13 11:32:32 -0300

    refactor: improve routing and HTTP logic

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { CreateRoom } from "./pages/create-room";
-import { Room } from "./pages/room";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { RecordRoomAudio } from './pages/record-room-audio'
+import { Room } from './pages/room'
 
 const queryClient = new QueryClient()
 
@@ -9,9 +10,10 @@ export function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <Routes> 
+        <Routes>
           <Route element={<CreateRoom />} index />
           <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
